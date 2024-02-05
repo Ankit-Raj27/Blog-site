5fdce6f88c6c1096b999bb8761152978c4b88381
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-02-06 01:05:43 +0530

    [A blog site]

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// import { useState } from 'react'
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return <>What up bitch</>;
+}
+
+export default App;
